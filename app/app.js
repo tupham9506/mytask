5685838cb73c4d6579889552b1db932f2aae2cce
tuszx <phambaotuszx@gmail.com>
2017-08-08 06:02:06 +0700

    init index

---
+++ app/app.js
@@ -0,0 +1,21 @@
+// import header
+document.write('<!DOCTYPE html><html><head><title>Công việc của bạn</title><meta charset="UTF8"/></head><body></body></html>');
+
+// import db
+// document.write('<script type="text/javascript" src="database/lokijs.js"></script>');
+// document.write('<script type="text/javascript" src="database/connection.js"></script>');
+
+
+// init jquery
+document.write('<script type="text/javascript" src="../lib/jquery/jquery.min.js"></script>');
+
+// import css lib
+document.write('<link rel= "stylesheet" type="text/css" href="../lib/bootstrap/css/bootstrap.min.css">');
+document.write('<link rel= "stylesheet" type="text/css" href="../lib/daterangepicker/daterangepicker.css">');
+document.write('<link rel= "stylesheet" type="text/css" href="../css/style.css">');
+document.write('<link rel= "stylesheet" type="text/css" href="../css/const.css">');
+
+// import lib	
+document.write('<script type="text/javascript" src="../lib/bootstrap/js/bootstrap.min.js"></script>');
+document.write('<script type="text/javascript" src="../lib/daterangepicker/daterangepicker.js"></script>');
+
