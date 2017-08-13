52b0a8f1d147e383bfebab56e9b4e047ac31493b
tuszx <phambaotuszx@gmail.com>
2017-08-13 23:46:33 +0700

    remove bootstrap,loki lib

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
 // import header
-document.write('<!DOCTYPE html><html><head><title>Công việc của bạn</title><meta charset="UTF8"/></head><body></body></html>');
+document.write('<!DOCTYPE html><html><head><title>Công việc của bạn</title><meta charset="UTF8"/><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></head><body></body></html>');
 
 // import db
-// document.write('<script type="text/javascript" src="database/lokijs.js"></script>');
-// document.write('<script type="text/javascript" src="database/connection.js"></script>');
+document.write('<script type="text/javascript" src="../lib/loki/lokijs.min.js"></script>');
+document.write('<script type="text/javascript" src="../connect.js"></script>');
 
 
 // init jquery
@@ -17,5 +17,6 @@ document.write('<link rel= "stylesheet" type="text/css" href="../css/const.css">
 
 // import lib	
 document.write('<script type="text/javascript" src="../lib/bootstrap/js/bootstrap.min.js"></script>');
+document.write('<script type="text/javascript" src="../lib/moment/moment.min.js"></script>');
 document.write('<script type="text/javascript" src="../lib/daterangepicker/daterangepicker.js"></script>');
 
--- app/database/connection.js
+++
@@ -1,1 +0,0 @@
-var db = new loki('mytask');
